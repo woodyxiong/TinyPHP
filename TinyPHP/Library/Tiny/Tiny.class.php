1c83c9d2c4ee475abda506d743882a8833a9eb1a
woody <535347614@qq.com>
2017-04-05 10:29:40 +0800

    第二次从phpstorm提交

--- TinyPHP/Library/Tiny/Tiny.class.php
+++ TinyPHP/Library/Tiny/Tiny.class.php
@@ -1,2 +1,30 @@
 <?php
-echo "string";
+namespace Tiny;
+class Tiny{
+    //类映射
+    private static $_map=array();
+
+    /**
+     * 应用程序初始化
+     */
+    public static function begin(){
+        //注册AUTOLOAD方法
+        spl_autoload_register('Tiny\Tiny::autoload');
+        //设定错误和异常处理
+
+        include TINY_COMMON.'function.php';
+        T('loadTime');
+    }
+
+    public static function autoload($class){
+        //是否存在类映射
+        if(isset(self::$_map[$class])){
+            include self::$_map[$class];
+        }elseif(false!==strpos($class,'\\')){
+            $name=strstr($class,'\\',true);
+            $filename=TINY_LIB.$class.EXT;
+            if(is_file($filename))
+                include $filename;
+        }
+    }
+}
