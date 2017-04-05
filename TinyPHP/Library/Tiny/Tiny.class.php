ef7ed16c80fb6ec9d8a46e7af6718528f5b1e804
woody <535347614@qq.com>
2017-04-05 18:21:52 +0800

    搞了一天也不知道谢了啥

--- TinyPHP/Library/Tiny/Tiny.class.php
+++ TinyPHP/Library/Tiny/Tiny.class.php
@@ -11,9 +11,13 @@ class Tiny{
         //注册AUTOLOAD方法
         spl_autoload_register('Tiny\Tiny::autoload');
         //设定错误和异常处理
+        error_reporting(E_ALL || ~E_NOTICE); //显示除去 E_NOTICE 之外的所有错误信息
 
         include TINY_COMMON.'function.php';
         T('loadTime');
+
+        //运行应用
+        App::run();
     }
 
     public static function autoload($class){
