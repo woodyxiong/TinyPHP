3f25b08adeb076bd983f14d82791efd1cfe6e34b
woody <535347614@qq.com>
2017-08-06 12:32:35 +0800

    完成对页面的渲染并输出到浏览器

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Tiny\Controller;
 class IndexController extends Controller{
     public function index(){
+        $this->assign("xk","shuai");
         $this->display();
     }
 
--- TinyPHP/Library/Tiny/View.class.php
+++ TinyPHP/Library/Tiny/View.class.php
@@ -4,6 +4,7 @@ namespace Tiny;
  * Tiny 视图类
  */
 class View{
+
     protected $tVar=array();
 
     /**
@@ -25,16 +26,59 @@ class View{
      */
     public function display($templateFile=''){
         //获得模板路径
+        $templatePath=$this->fetch($templateFile);
+        //获得模板数据
+        $content=$this->getContent($templatePath);
+        //渲染模板
+        $this->render($content);
+    }
+
+    /**
+     * 获得模板
+     * @param  string $templateFile 模板文件名
+     * @return string $templatePath 模板实际路径
+     */
+    private function fetch($templateFile=''){
         $controller_name=ucfirst(CONTROLLER_NAME);
+        $templatePath='';
         if(empty($templateFile)){
-            $templateFile=APP_HOME.'View/'.$controller_name.'/'.ACTION_NAME.'.html';
+            $templatePath=APP_HOME.'View/'.$controller_name.'/'.ACTION_NAME.'.html';
         }else{
-            $templateFile=APP_HOME.'View/'.$controller_name.'/'.$templateFile.'.html';
+            $templatePath=APP_HOME.'View/'.$controller_name.'/'.$templateFile.'.html';
         }
         //找不到模板
-        if(!is_file($templateFile)) exit("can not find templatefile");
+        if(!is_file($templatePath)) exit("can not find templatefile");
+        return $templatePath;
+    }
 
-        
+    /**
+     * 获得输出到浏览器的文本
+     * @param  string $templatePath 模板路径
+     * @return [type]               文本内容
+     */
+    private function getContent($templatePath){
+        //开始页面缓存
+        ob_start();
+        //强制不输出到浏览器
+        ob_implicit_flush(0);
+        //拆分变量
+        extract($this->tVar,EXTR_OVERWRITE);
+        include $templatePath;
+        //获得从浏览器的输出
+        $content=ob_get_clean();
+        return $content;
+    }
+
+    /**
+     * 添加header，渲染html页面
+     * @param  string $content 模板内容
+     */
+    private function render($content){
+        // 网页字符编码
+        header('text/html;charset=UTF-8');
+        header('X-Powered-By: tinyPHP');
+        // 输出模板文件
+        echo $content;
     }
 
 }
