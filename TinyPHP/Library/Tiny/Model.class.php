02363c462132bbcb8bfdc8c90673524a66fd485b
woody <zuosixiaoshenxian@gmail.com>
2017-08-09 15:57:08 +0800

    引入PDO驱动

--- TinyPHP/Library/Tiny/Model.class.php
+++ TinyPHP/Library/Tiny/Model.class.php
@@ -1,10 +1,23 @@
 <?php
 namespace Tiny;
+use PDO;
 class Model{
+    private $db=null;
     // 构造函数
-    public function __construct($name) {
-        $this->_db['name']=$name;
-        $this->db(0);
+    public function __construct($config) {
+        //仅支持mysql数据库
+        if(!in_array($config['DB_TYPE'],array('mysql'))){
+            exit('can not support '.$config['DB_TYPE'].' DATABASE');
+        }
+        try{
+            $dsn=$config['DB_TYPE'].":host=".$config['DB_TYPE'].";dbname=".$config['DB_PWD'];
+            echo "pdo1";
+            $db=new PDO($dsn,$config['DB_USER'],$config['DB_PWD']);
+            echo "pdo2";
+        }catch(\PDOException $e){
+            die ($e->getMessage());
+        }
+
     }
 
 }
